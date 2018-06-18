df4eafa45a7d372796c3e0e83d87f185306b6a9b
Wolfgang Schuster <wolfadex@gmail.com>
2018-06-18 16:28:06 -0500

    Main code commit

---
+++ src/Main.elm
@@ -0,0 +1,195 @@
+module Main exposing (main)
+
+
+import Css
+-- import Debug exposing (log)
+import Dom
+import Html.Styled as Html exposing (Html)
+import Html.Styled.Attributes as Attribute
+import Html.Styled.Events as Event
+import Keyboard
+import Keyboard.Extra as Keys
+import Mouse exposing (Position)
+import Rope exposing (Rope(..))
+import Task
+
+
+---- MODEL ----
+
+
+type alias Model =
+    { content : Rope
+    , cursor : Position
+    }
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( { content = Empty
+      , cursor = Position 0 0
+      }
+    , Cmd.none
+    )
+
+
+
+---- UPDATE ----
+
+
+type Msg
+    = NoOp
+    | FocusContent
+    | FocusContentResult (Result Dom.Error ())
+    | CursorMove Position
+    | TextInsert String
+    | TextBackSpace
+    | TextDelete
+    | TextRemoveHighlighted
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        CursorMove amount ->
+            ( { model | cursor = moveCursor model.cursor amount (Rope.getLength model.content) }
+            , Cmd.none
+            )
+        TextInsert text ->
+            let
+                newContent = Rope.insert text model.cursor.x model.content
+            in
+                ( { model
+                  | content = newContent
+                  , cursor = moveCursor model.cursor (Position (String.length text) 0) (Rope.getLength newContent)
+                  }
+                , Cmd.none
+                )
+        TextBackSpace ->
+            let
+                cursorX = model.cursor.x
+                newContent = Rope.remove (cursorX - 1) cursorX model.content
+            in
+                ( { model
+                  | content = newContent
+                  , cursor = moveCursor model.cursor (Position -1 0) (Rope.getLength newContent)
+                  }
+                , Cmd.none
+                )
+        -- TextDelete ->
+        --
+        -- TextRemoveHighlighted ->
+        --
+        FocusContent ->
+            ( model, Task.attempt FocusContentResult (Dom.focus "editor-content") )
+        FocusContentResult result ->
+            case result of
+                Err (Dom.NotFound id) ->
+                    ( model, Cmd.none )
+                Ok () ->
+                    ( model, Cmd.none )
+        NoOp ->
+            ( model, Cmd.none )
+        _ ->
+            ( model, Cmd.none )
+
+
+moveCursor : Position -> Position -> Int -> Position
+moveCursor { x, y } amount maxX =
+    let
+        newX = min (x + amount.x) maxX
+        newY = y + amount.y
+    in
+        Position (max 0 newX) newY
+
+
+---- VIEW ----
+
+
+view : Model -> Html Msg
+view { content, cursor } =
+    Html.div [ Attribute.css [ Css.displayFlex
+                             , Css.flexDirection Css.column
+                             , Css.position Css.absolute
+                             , Css.left (Css.px 0)
+                             , Css.right (Css.px 0)
+                             , Css.top (Css.px 0)
+                             , Css.bottom (Css.px 0)
+                             ]
+             ]
+             [ contentArea content cursor
+             , Html.input [ Attribute.id "editor-content"
+                          , Event.onInput TextInsert
+                          , Attribute.value ""
+                          , Attribute.css [ Css.opacity (Css.num 0)
+                                          , Css.height (Css.rem 0)
+                                          , Css.border (Css.rem 0)
+                                          , Css.padding (Css.rem 0)
+                                          ]
+                          ]
+                          []
+             , Html.text ((toString cursor.x) ++ ", " ++ (toString cursor.y))
+             ]
+
+
+contentArea : Rope -> Position -> Html Msg
+contentArea content cursor =
+    Html.div [ Event.onClick FocusContent
+             , Attribute.css [ Css.minHeight (Css.rem 5)
+                             , Css.backgroundColor (Css.rgb 200 200 200)
+                             , Css.flex (Css.num 1)
+                             ]
+             ]
+             [ Html.text <| Rope.toString content
+             , cursorNode
+             ]
+
+
+cursorNode : Html Msg
+cursorNode =
+    Html.div [ Attribute.css [ Css.height (Css.rem 1)
+                             , Css.width (Css.rem 0.8)
+                             , Css.borderLeftStyle Css.solid
+                             , Css.borderLeftColor (Css.rgb 0 0 0)
+                             , Css.borderLeftWidth (Css.rem 0.1)
+                             , Css.marginLeft (Css.rem 1) -- TODO: temp only
+                             ]
+             , Attribute.class "cursor"
+             ]
+             []
+
+
+---- PROGRAM ----
+
+
+main : Program Never Model Msg
+main =
+    Html.program
+        { view = view
+        , init = init
+        , update = update
+        -- , subscriptions = always Sub.none
+        , subscriptions = subscriptions
+        }
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.batch [ Keyboard.downs handleKeyDown
+              ]
+
+
+-- handleKeyDown : KeyCode -> Sub Msg
+handleKeyDown code =
+    case (Keys.fromCode code) of
+        Keys.BackSpace ->
+            TextBackSpace
+        Keys.ArrowRight ->
+            CursorMove (Position 1 0)
+        Keys.ArrowLeft ->
+            CursorMove (Position -1 0)
+        Keys.ArrowUp ->
+            CursorMove (Position 0 -1)
+        Keys.ArrowDown ->
+            CursorMove (Position 0 1)
+        _ ->
+            NoOp
---
+++ src/Rope.elm
@@ -0,0 +1,126 @@
+module Rope exposing (Rope(..), toString, insert, remove, getLength)
+
+
+type Rope
+    = Empty
+    | Leaf String Int
+    | Node Rope Rope Int
+
+
+splitLength : Int
+splitLength = 4
+
+
+joinLength : Int
+joinLength = 2
+
+
+rebalanceRatio : Float
+rebalanceRatio = 1.2
+
+
+adjust : Rope -> Rope
+adjust rope =
+    case rope of
+        Empty ->
+            Empty
+        Leaf string length ->
+            if length > splitLength then
+                let
+                    midPoint = length // 2
+                    leftString = String.left midPoint string
+                    rightString = String.dropLeft midPoint string
+                    leftRope = adjust (Leaf leftString midPoint)
+                    rightLength = length - midPoint
+                    rightRope = if rightLength > 0 then
+                                    adjust (Leaf rightString rightLength)
+                                else
+                                    Empty
+                in
+                    Node leftRope rightRope midPoint
+            else
+                rope
+        Node left right leftLength ->
+            if leftLength < joinLength then
+                Leaf (toString left ++ (toString right)) leftLength
+            else
+                rope
+
+
+toString : Rope -> String
+toString rope =
+    case rope of
+        Empty ->
+           ""
+        Leaf string _ ->
+           string
+        Node left right _ ->
+           (toString left) ++ (toString right)
+
+
+insert : String -> Int -> Rope -> Rope
+insert text position rope =
+    case rope of
+        Empty ->
+            adjust (Leaf text (String.length text))
+        Leaf string _ ->
+            let
+                newText = String.concat [ String.left position string
+                                        , text
+                                        , String.dropLeft position string
+                                        ]
+            in
+                adjust <| Leaf newText (String.length newText)
+        Node left right leftLength ->
+            if position < leftLength then
+                Node (insert text position left) right (leftLength + (String.length text))
+            else
+                Node left (insert text position right) leftLength
+
+
+remove : Int -> Int -> Rope -> Rope
+remove from to rope =
+    case rope of
+        Empty ->
+            Empty
+        Leaf string length ->
+            let
+                l = String.left from string
+                r = String.dropLeft to string
+                newText = l ++ r
+            in
+                Leaf newText (String.length newText)
+        Node left right leftLength ->
+            -- rope
+            let
+                leftStart = min from leftLength
+                leftEnd = min to leftLength
+                rightLength = getLength right
+                rightStart = max 0 (min (from - leftLength) rightLength)
+                rightEnd = max 0 (min (to - leftLength) rightLength)
+                newLeft = if leftStart < leftLength then remove leftStart leftEnd left else left
+                newRight = if rightEnd > 0 then remove rightStart rightEnd right else right
+            in
+                adjust <| Node newLeft newRight (getLength newLeft)
+
+
+getLength : Rope -> Int
+getLength rope =
+    case rope of
+        Empty ->
+            0
+        Leaf _ length ->
+            length
+        Node _ right leftLength ->
+            getLengthHelper leftLength right
+
+
+getLengthHelper : Int -> Rope -> Int
+getLengthHelper lengthSoFar rope =
+    case rope of
+        Empty ->
+            lengthSoFar
+        Leaf _ length ->
+            lengthSoFar + length
+        Node _ right leftLength ->
+            getLengthHelper (lengthSoFar + leftLength) right
